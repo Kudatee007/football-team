0e2ef3bf54484d87f6182e3c17b099c377366f3a
kudaT <timilehinkudaisi01@gmail.com>
2023-02-22 12:37:08 +0100

    added number filters

--- controller/teamController.js
+++ controller/teamController.js
@@ -2,7 +2,7 @@ const Teams = require("../model/teamModel");
 
 const getAllTeams = async (req, res) => {
   console.log(req.query);
-  const { name, location, ulcwinner, sort, select } = req.query;
+  const { name, location, ulcwinner, sort, select, numberFilters } = req.query;
   let queryObject = {};
   let result = Teams.find(queryObject);
   if (name) {
@@ -16,24 +16,45 @@ const getAllTeams = async (req, res) => {
   if (ulcwinner) {
     queryObject.ulcwinner = ulcwinner === "true" ? true : false;
   }
+  if (numberFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    // rejex for mathematical comparison/conversion
+    const regEx = /\b(<|>|>=|<=|=)\b/g;
 
+    let filters = numberFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+
+    const options = ["rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [search, operator, value] = item.split("-");
+      if (options.includes(search)) {
+        queryObject[search] = { [operator]: Number(value)};  
+      }
+    });
+  }
   //SORTING
   if (sort) {
-    const sortList = sort.split(",").join(" ")
-    result = result.sort(sortList)
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
   }
 
   // SELECT
   if (select) {
-    const selectList = select.split(",").join(" ")
-    result = result.select(selectList)
-  };
+    const selectList = select.split(",").join(" ");
+    result = result.select(selectList);
+  }
 
   // LIMIT
   const limit = Number(req.query.limit);
   result = result.limit(limit);
-
-
 
   result = result.find(queryObject);
   const team = await result;
@@ -41,7 +62,8 @@ const getAllTeams = async (req, res) => {
     success: true,
     no0fTeams: team.length,
     team,
-  });[]
+  });
+  [];
 };
 
 module.exports = getAllTeams;
