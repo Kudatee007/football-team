301512f5b9403ccd97b66059d63c98ac74ddd141
kudaT <timilehinkudaisi01@gmail.com>
2023-02-23 12:07:22 +0100

    added cors

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 require("dotenv").config()
 const express = require("express");
 const app = express();
-const PORT = 6300;
+const PORT = process.env.PORT || 6300;
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', true);
 const teamRouter = require("./routes/teamRouter")
-const notFound = require("./middleware/notFound")
+const notFound = require("./middleware/notFound");
+const cors = require("cors");
 
 
-
-
+app.use(cors())
+app.use(express.json())
 app.use(teamRouter)
 
 
